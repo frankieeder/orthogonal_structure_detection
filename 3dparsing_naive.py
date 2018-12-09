d4cc03ccd3b008062ab6ffa2f7aa13111dceb967
Frankie Eder <frankieeder129@gmail.com>
2018-12-09 13:47:47 -0800

    Going to change function structure to allow me to... see results maybe.

--- 3dparsing_naive.py
+++ 3dparsing_naive.py
@@ -4,30 +4,33 @@ import matplotlib.pyplot as plt
 import cv2 as cv
 import os
 import time
+import re
 from scipy.spatial import Voronoi, voronoi_plot_2d
 from mpl_toolkits.mplot3d import Axes3D
 
 root = './s3dis'
 subfolders = lambda dir: next(os.walk(dir))[1]
-areas = subfolders(root)
-areas = [root + '/' + a for a in areas]
-areas = [[a + '/' + r + '/' + r + ".txt" for r in subfolders(a)] for a in areas]
+areas = [root + '/' + a for a in subfolders(root)]
+area_rooms = [[a + '/' + r + '/' + r + ".txt" for r in subfolders(a)] for a in areas]
+structure_pcs = []
 
+def pc_to_csv(file):
+    return pd.read_csv(
+            filepath_or_buffer=file,
+            sep=' ',
+            names=['x', 'y', 'z', 'r', 'g', 'b'])
 
 pcs = []
 area_1_dir = './area_1_pc'
 """if not os.path.isfile(area_1_dir):"""
-for area in areas[:1]:
-    area_rooms = []
+for area in area_rooms[:1]:
+    all_rooms = []
+    structure_room = []
     for room in area[:]:
         print(room)
-        room_df = pd.read_csv(
-            filepath_or_buffer=room,
-            sep=' ',
-            names=['x', 'y', 'z', 'r', 'g', 'b']
-        )
-        area_rooms.append(room_df)
-    area_reconstruction = pd.concat(area_rooms)
+        room_df = pc_to_csv(room)
+        all_rooms.append(room_df)
+    area_reconstruction = pd.concat(all_rooms)
     pcs.append(area_reconstruction)
 pc = pcs[0]
 
@@ -69,8 +72,8 @@ def plot_cloud(pc):
 def find_perpendicular_structures(pc, a1, a2, structure_title):
     #plot_cloud(pc.sample(frac=0.01))
     start = time.time()
-    min_a1, max_a1 = min(pc[a1]), max(pc[a1])
-    min_a2, max_a2 = min(pc[a2]), max(pc[a2])
+    min_a1, max_a1 = pc[a1].min(), pc[a1].max()
+    min_a2, max_a2 = pc[a2].min(), pc[a2].max()
     a1n = a1 + '_pix'
     a2n = a2 + '_pix'
     pc[a1n] = (pc[a1] - min_a1) // PIXEL_SIZE
@@ -137,12 +140,23 @@ def find_perpendicular_structures(pc, a1, a2, structure_title):
     merged[structure_title] = merged[structure_title].fillna(False)
     print("Final pc cleanup: {0}".format(time.time() - start))
     return merged
-
-
     #vor = Voronoi(wall_points)
     #voronoi_plot_2d(vor)
 pc = find_perpendicular_structures(pc, 'x', 'z', 'floor_x')
 pc = find_perpendicular_structures(pc, 'y', 'z', 'floor_y')
 pc = find_perpendicular_structures(pc, 'x', 'y', 'wall')
+pc['floor'] = np.logical_or(pc['floor_x'], pc['floor_y'])
+pc['structure'] = np.logical_or(pc['floor'], pc['wall'])
+detected = pc[pc['structure']]
+
+area_structures = []
+for area in areas[:1]:
+    files = []
+    for (dirpath, dirnames, filenames) in os.walk(area):
+        files.extend(dirpath + "/" + f for f in filenames)
+    structure_pcs = [f for f in files if "wall" in f or "ceiling" in f or "floor" in f]
+    area_structures.append(structure_pcs)
+structure_df = pd.concat(pc_to_csv(f) for f in structure_pcs)
+
 
 plt_grey(x_y)
