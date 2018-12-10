d630b8862b7fb4b436fc910e974b1864bb4b87c6
Frankie Eder <frankieeder129@gmail.com>
2018-12-09 18:05:38 -0800

    I think I fixed the transpose problem, and am getting actual IOU results.

--- 3dparsing_naive.py
+++ 3dparsing_naive.py
@@ -10,48 +10,39 @@ from mpl_toolkits.mplot3d import Axes3D
 
 root = './s3dis'
 subfolders = lambda dir: next(os.walk(dir))[1]
-areas = [root + '/' + a for a in subfolders(root)]
-#area_rooms = [[a + '/' + r + '/' + r + ".txt" for r in subfolders(a)] for a in areas]
 structure_pcs = []
 
-def pc_to_csv(file):
+PIXEL_SIZE = 0.5 / 39.37  # 1 inches
+THRESHOLD = 25
+
+def pc_to_df(file):
     return pd.read_csv(
             filepath_or_buffer=file,
             sep=' ',
             names=['x', 'y', 'z', 'r', 'g', 'b'])
 
-area_dfs = {}
-for area in areas[:1]:
-    room_dfs = {}
-    for room in subfolders(area):
-        print(room)
-        this_room_dir = area + '/' + room
-        room_full_df = pc_to_csv(this_room_dir + '/' + room + ".txt")
-        annotations_dir, subdirs, annotated_files = next(os.walk(this_room_dir + "/Annotations"))
-        annotations_files = [annotations_dir + "/" +  f for f in annotated_files]
-        room_structure_df = \
-            pd.concat(pc_to_csv(f) for f in annotations_files if "wall" in f or "ceiling" in f or "floor" in f)
-        room_dfs.update({room: (room_full_df, room_structure_df)})
-    area_dfs.update(room_dfs)
-
-x = 2
-
-main_pcs = []
-area_1_dir = './area_1_pc'
-"""if not os.path.isfile(area_1_dir):"""
-for area in area_rooms[:1]:
-    all_rooms = []
-    structure_room = []
-    for room in area[:1]:
-        print(room)
-        room_df = pc_to_csv(room)
-        all_rooms.append(room_df)
-    area_reconstruction = pd.concat(all_rooms)
-    main_pcs.append(area_reconstruction)
-pc = pcs[0]
-
-PIXEL_SIZE = 0.5 / 39.37  # 1 inches
-THRESHOLD = 25
+def get_data(root):
+    print("Loading Data...")
+    area_dfs = []
+    for area in subfolders(root)[:1]:
+        room_dfs = []
+        for room in subfolders(root + '/' + area)[:1]:
+            print(room)
+            this_room_dir = root + "/" + area + '/' + room
+            annotations_dir, subdirs, annotated_files = next(os.walk(this_room_dir + "/Annotations"))
+            annots_df = []
+            for file in annotated_files:
+                df = pc_to_df(annotations_dir + "/" + file)
+                df['annotation'] = file[:-4]
+                annots_df.append(df)
+            room_df = pd.concat(annots_df)
+            room_df['room'] = room
+            room_dfs.append(room_df)
+        area_df = pd.concat(room_dfs)
+        area_df['area'] = area
+        area_dfs.append(area_df)
+    pc = pd.concat(area_dfs)
+    return pc
 
 def plt_grey(img):
     plt.imshow(img, cmap="gray")
@@ -72,28 +63,31 @@ def plot_cloud(pc):
         pc['z'],
         s=0.5
     )
+
+def pixelize_and_plot_pc(pc, a1, a2):
+    min_a1, max_a1 = pc[a1].min(), pc[a1].max()
+    min_a2, max_a2 = pc[a2].min(), pc[a2].max()
+    bins = [(max_a1 - min_a1) // PIXEL_SIZE, (max_a2 - min_a2) // PIXEL_SIZE]
+    img = plt.hist2d(x=pc[a1], y=pc[a2], bins=bins)[0]
+    return img.T.copy()
+
 def find_perpendicular_structures(pc, a1, a2, structure_title, merge_type='left'):
     #plot_cloud(pc.sample(frac=0.01))
     start = time.time()
-    min_a1, max_a1 = pc[a1].min(), pc[a1].max()
-    min_a2, max_a2 = pc[a2].min(), pc[a2].max()
     a1n = a1 + '_pix'
     a2n = a2 + '_pix'
-    pc[a1n] = (pc[a1] - min_a1) // PIXEL_SIZE
-    pc[a2n] = (pc[a2] - min_a2) // PIXEL_SIZE
-    bins = [(max_a1 - min_a1) // PIXEL_SIZE, (max_a2 - min_a2) // PIXEL_SIZE]
+    pc[a1n] = (pc[a1] - pc[a1].min()) // PIXEL_SIZE
+    pc[a2n] = (pc[a2] - pc[a2].min()) // PIXEL_SIZE
     print("Prep time: {0}".format(time.time() - start))
     start = time.time()
-    plot = plt.hist2d(x=pc[a1], y=pc[a2], bins=bins)
+    x_y = pixelize_and_plot_pc(pc, a1, a2)
     print("Image Creation: {0}".format(time.time() - start))
-    start = time.time()
     plt.clf()
     plt.close()
-    print("Plot closing: {0}".format(time.time() - start))
     start = time.time()
-    x_y = plot[0]
     x_y = normalize_image(x_y)
     x_y = x_y.astype(np.uint8)
+    #plt_grey(x_y)
     print("Image extraction and normalization: {0}".format(time.time() - start))
     start = time.time()
     #image = image.astype(np.float32)
@@ -145,21 +139,30 @@ def find_perpendicular_structures(pc, a1, a2, structure_title, merge_type='left'
     return merged
     #vor = Voronoi(wall_points)
     #voronoi_plot_2d(vor)
-pc = find_perpendicular_structures(pc, 'x', 'z', 'floor_x')
-pc = find_perpendicular_structures(pc, 'y', 'z', 'floor_y')
+
+def IOU(a, b):
+    return sum(a & b) / sum(a | b)
+
+pc = get_data(root)
+#pc = find_perpendicular_structures(pc, 'x', 'z', 'floor_x')
+#pc = find_perpendicular_structures(pc, 'y', 'z', 'floor_y')
 pc = find_perpendicular_structures(pc, 'x', 'y', 'wall')
-pc['floor'] = np.logical_or(pc['floor_x'], pc['floor_y'])
-pc['structure'] = np.logical_or(pc['floor'], pc['wall'])
-detected = pc[pc['structure']]
+actual_wall = pc.annotation.str.contains('wall')
+actual_door = pc.annotation.str.contains('door')
+actual_vertical_surfaces = actual_wall | actual_door
+#actual_floor = pc.annotation.str.contains('floor')
+#actual_ceiling = pc.annotation.str.contains('ceiling')
+#actual_structure = actual_wall | actual_floor | actual_ceiling
+#pred_structure = pc['floor_x'] | pc['floor_y'] | pc['wall']
 
-area_structures = []
-for area in areas[:1]:
-    files = []
-    for (dirpath, dirnames, filenames) in os.walk(area):
-        files.extend(dirpath + "/" + f for f in filenames)
-    structure_pcs = [f for f in files if "wall" in f or "ceiling" in f or "floor" in f]
-    area_structures.append(structure_pcs)
-#structure_df = pd.concat(pc_to_csv(f) for f in structure_pcs)
+all = pixelize_and_plot_pc(pc, 'x', 'y')
+plt.clf()
+plt.close()
 
+actual_wall = pixelize_and_plot_pc(pc[actual_wall], 'x', 'y')
+plt.clf()
+plt.close()
 
+wall_IOU = IOU(actual_vertical_surfaces, pc['wall'])
+print(wall_IOU)
 plt_grey(x_y)
